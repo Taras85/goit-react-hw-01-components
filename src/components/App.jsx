189b059c2ca2636d12bdd8ca17ba793b761f9123
Taras <topanasenko85@gmail.com>
2023-04-28 23:27:02 +0300

    full

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -4,8 +4,8 @@ import { Statistics } from './Statistics/Statistics.jsx'
 import data from './Statistics/data.json'
 import { FriendList } from './FriendList/FriendList.jsx'
 import friends from './FriendList/friends.json'
-// import { HystoryTransactions } from './HystoryTransactions/HystoryTransactions.jsx'
-// import transactions from './HystoryTransactions/transactions.json'
+import { HistoryTransactions } from './HistoryTransactions/HistoryTransactions.jsx'
+import transactions from './HistoryTransactions/transactions.json'
 
 
 export const App = () => {
@@ -23,6 +23,7 @@ export const App = () => {
       <Profile user ={user}/>
       <Statistics data={data} title = "Hello"/>
       <FriendList friends={friends} />
+      <HistoryTransactions items={transactions} />
 
     
     </div>
---
+++ src/components/HistoryTransactions/HistoryTransactions.jsx
@@ -0,0 +1,28 @@
+
+import History from './HistoryTransactions.module.css';
+import PropTypes from 'prop-types';
+
+export const HistoryTransactions = ({ items }) => {
+    return (
+        <table className={History.transactionHistory}>
+            <thead>
+                <tr>
+                    <th className={History.event}>Type</th>
+                    <th className={History.event}>Amount</th>
+                    <th className={History.event}>Currency</th>
+                </tr>
+            </thead>
+            <tbody>
+                {items.map(({ id, type, amount, currency }) => (
+                <tr key={id} className={History.tr}>
+                    <th className={History.th}>{type}</th>
+                    <th className={History.th}>{amount}</th>
+                    <th className={History.th}>{currency}</th>
+                </tr>))}
+            </tbody>
+        </table>)
+}
+
+HistoryTransactions.propTypes = {
+  items: PropTypes.object.isRequired,
+}
