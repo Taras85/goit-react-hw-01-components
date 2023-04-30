743108fb675aefa3503bda9b6f3004c8b4675bb7
Taras <topanasenko85@gmail.com>
2023-04-30 20:39:02 +0300

    fix

--- src/components/FriendList/FriendListItem.jsx
+++ src/components/FriendList/FriendListItem.jsx
@@ -2,8 +2,8 @@ import PropTypes from 'prop-types';
 import styles from './FriendList.module.css'
 
 
-export function FriendListItem({avatar, name, isOnline, id}) {
-    return  <li className={styles.item} key = {id}>
+export function FriendListItem({avatar, name, isOnline, }) {
+    return  <li className={styles.item} >
             <span className={styles.status} style = {{backgroundColor: isOnline ? 'green':'red'}}></span>
             <img className={styles.avatar} src={avatar} alt={name} width="48" />
             <p className={styles.name}>{ name }</p>
@@ -14,5 +14,5 @@ FriendListItem.propTypes = {
     avatar: PropTypes.string.isRequired,
     name: PropTypes.string.isRequired,
     isOnline: PropTypes.bool.isRequired,
-    id: PropTypes.number.isRequired,
+   
 }
